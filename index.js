4188d3274585940ae433a37d724f7ce167bfdf20
Joshua Zuo <neveragain8209@yahoo.com>
2018-03-16 08:33:55 -0500

    added google map

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
+function getLatLng() {
 
+}
+
+ function initMap() {
+        let map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: -25.363, lng: 131.044},
+          zoom: 4
+        });
+      }
 
 function showApp() { 
  $('#hideIntro').click( () => {
