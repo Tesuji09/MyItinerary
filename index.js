3e2ca842e67c770834f3ba77b23d4c24c1044d76
Joshua Zuo <neveragain8209@yahoo.com>
2018-03-18 12:28:16 -0500

    started right side search bar

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-const FOURSQUARE_URL = 'https://api.foursquare.com/v2/venues/explore'
+const FOURSQUARE_URL = 'https://api.foursquare.com/v2/venues/search'
 
 
 function getFourSquareData(callback) {
 	const query = {
-		client_id:'LRGCN52GOXN3F3GNBI1CXNZSBUQWSRH4IJGWTO0FGEAZ5AVO',
-		client_secret:'ITVYE3R0XNXBQOW0RGIBEFFDKHH2ZD0LEFJTAVORDI2ZRDJK',
-		v:'20170801',
-		ll:`41.8781, -87.6298}`,
-		query:'coffee',
-		limit:1
+		client_id: 'LRGCN52GOXN3F3GNBI1CXNZSBUQWSRH4IJGWTO0FGEAZ5AVO',
+	    client_secret: 'ITVYE3R0XNXBQOW0RGIBEFFDKHH2ZD0LEFJTAVORDI2ZRDJK',
+	    ll: '41.8781,-87.6298',
+	    query: 'coffee',
+	    v: '20170801',
+	    limit: 5
 	};
 	$.getJSON(FOURSQUARE_URL, query, callback);
 }
@@ -37,7 +37,7 @@ function showData(data) {
  function initMap() {
         let map = new google.maps.Map(document.getElementById('map'), {
           center: {lat: 41.8781, lng: -87.6298 },
-          zoom: 10
+          zoom: 11
         });
       }
 
