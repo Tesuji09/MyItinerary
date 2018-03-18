b93296422cebd57d9c9f9a42bd8b517690e5abea
Joshua Zuo <neveragain8209@yahoo.com>
2018-03-18 10:40:17 -0500

    shows foursquare data in console

--- index.js
+++ index.js
@@ -1,18 +1,50 @@
-function getLatLng() {
+const FOURSQUARE_URL = 'https://api.foursquare.com/v2/venues/explore'
 
+
+function getFourSquareData(callback) {
+	const query = {
+		client_id:'LRGCN52GOXN3F3GNBI1CXNZSBUQWSRH4IJGWTO0FGEAZ5AVO',
+		client_secret:'ITVYE3R0XNXBQOW0RGIBEFFDKHH2ZD0LEFJTAVORDI2ZRDJK',
+		v:'20170801',
+		ll:`41.8781, -87.6298}`,
+		query:'coffee',
+		limit:1
+	};
+	$.getJSON(FOURSQUARE_URL, query, callback);
 }
+
+function showData(data) {
+	console.log(data);
+}
+
+
+// This section will be for handling the map
+
+
+// function getLat() {
+// 	navigator.geolocation.getCurrentPosition(position => {
+// 		return position.coords.latitude;
+// 	});
+// }
+
+// function getLng() {
+// 	navigator.geolocation.getCurrentPosition(position => {
+// 		return position.coords.longitude;
+// 	});
+// }
+
 
  function initMap() {
         let map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: -25.363, lng: 131.044},
-          zoom: 4
+          center: {lat: 41.8781, lng: -87.6298 },
+          zoom: 10
         });
       }
 
 function showApp() { 
- $('#hideIntro').click( () => {
+	$('#hideIntro').click( () => {
 	$('.introduction').fadeOut(1200);
 });
-}
+	getFourSquareData(showData)}
 
 $(showApp)   
